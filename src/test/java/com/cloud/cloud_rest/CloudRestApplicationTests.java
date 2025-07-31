fa628595c04ad5e419821d6c8474e8c9c932ca77
sonjiyoon12 <1wldbs1908@gmail.com>
2025-07-31 16:44:32 +0900

    first commit

---
+++ src/test/java/com/cloud/cloud_rest/CloudRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.cloud_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
