de94dd0aaeab8bf2768b75124d18adca27c97d5b
sonjiyoon12 <1wldbs1908@gmail.com>
2025-08-01 11:27:45 +0900

    일단 추가

---
+++ src/main/java/com/cloud/cloud_rest/resume/ResumeJpaRepository.java
@@ -0,0 +1,8 @@
+package com.cloud.cloud_rest.resume;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResumeJpaRepository extends JpaRepository<Resume, Long> {
+
+
+}
