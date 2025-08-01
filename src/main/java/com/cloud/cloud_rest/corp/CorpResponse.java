36e906caee3a641c84b7b4b21e133b4748af3836
whwwhwjddn123 <whwhwjddn123@gmail.com>
2025-08-01 18:31:26 +0900

    corpReponse

--- src/main/java/com/cloud/cloud_rest/corp/CorpResponse.java
+++ src/main/java/com/cloud/cloud_rest/corp/CorpResponse.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 public class CorpResponse {
 
+    // Corp 상세 정보
     @Data
     public static class CorpDTO{
         private Long corpId;
@@ -23,9 +24,31 @@ public class CorpResponse {
         }
     }
 
-    @Data
-    public static class SaveDTO{
 
+    // Corp 로그인 응답 DTO
+    @Data
+    public static class LoginDTO{
+        private String loginId;
+        private String corpName;
+
+        @Builder
+        public LoginDTO(Corp corp){
+            this.loginId = corp.getLoginId();
+            this.corpName = corp.getCorpName();
+        }
+    }
+
+    // Corp 업데이트 응답 DTO
+    @Data
+    public static class UpdateDTO{
+        private String corpName;
+        private String corpImage;
+
+        @Builder
+        public UpdateDTO(Corp corp,String corpUploadImage){
+            this.corpName = corp.getCorpName();
+            this.corpImage = corpUploadImage;
+        }
     }
 
 }
