5a12a4ceb09cf8579f318c8b299211e12b15f830
whwwhwjddn123 <whwhwjddn123@gmail.com>
2025-08-01 10:03:03 +0900

    gradle validation , passwordEncoder

---
+++ src/main/java/com/cloud/cloud_rest/user/User.java
@@ -0,0 +1,17 @@
+package com.cloud.cloud_rest.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "user_tb")
+@Builder
+public class User {
+}
