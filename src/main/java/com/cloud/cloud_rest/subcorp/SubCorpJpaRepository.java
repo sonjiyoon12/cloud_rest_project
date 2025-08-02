81dd256a74ca2ecfdb66bf8988bdec1b2c84ea08
Yangsb <ysb5397@gmail.com>
2025-08-02 10:08:59 +0900

    swagger api 추가, ãspring-boot 다운그래레ì이드, 오류 해결 등등

--- src/main/java/com/cloud/cloud_rest/subcorp/SubCorpJpaRepository.java
+++ src/main/java/com/cloud/cloud_rest/subcorp/SubCorpJpaRepository.java
@@ -5,7 +5,6 @@ import com.cloud.cloud_rest.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,13 +12,12 @@ import java.util.Optional;
 public interface SubCorpJpaRepository extends JpaRepository <SubCorp ,Long>{
 
     @Query("select s from SubCorp s join fetch s.user u where u.id = :userId")
-    List<SubCorp> findByUser(@Param("id") Long userId);
+    List<SubCorp> findByUser(@Param("userId") Long userId);
 
-    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END " +
+    @Query("SELECT CASE WHEN COUNT(s.user) > 0 THEN true ELSE false END " +
             "FROM SubCorp s WHERE s.user = :user AND s.corp = :corp")
     Optional<SubCorp> existsByUserAndCompany(@Param("user") User user, @Param("corp") Corp corp);
 
-    @Transactional
-    void deleteByUserIdAndCorpId(Long userId, Long corpId);
+    void deleteByUserUserIdAndCorpCorpId(Long userId, Long corpId);
 
 }
