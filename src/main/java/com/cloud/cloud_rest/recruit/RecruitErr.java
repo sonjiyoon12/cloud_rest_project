12accdf5430956f8f3106432703b7bfb4b716fd0
dovahk11m <choongechobiz@gmail.com>
2025-08-05 17:39:00 +0900

    recruit/skill 에러코드에 출처표시 ex) SkillErr: 해당 기술항목 없음 (조충희)

--- src/main/java/com/cloud/cloud_rest/recruit/RecruitErr.java
+++ src/main/java/com/cloud/cloud_rest/recruit/RecruitErr.java
@@ -8,10 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum RecruitErr {
 
-    CORP_NOT_FOUND("해당 기업 없음", HttpStatus.NOT_FOUND),
-    RECRUIT_NOT_FOUND("해당 공고 없음", HttpStatus.NOT_FOUND),
-    RECRUIT_FORBIDDEN("공고를 수정할 권한이 없습니다.", HttpStatus.FORBIDDEN);
-
+    CORP_NOT_FOUND("RecruitErr: 해당 기업 없음", HttpStatus.NOT_FOUND),
+    RECRUIT_NOT_FOUND("RecruitErr: 해당 공고 없음", HttpStatus.NOT_FOUND),
+    RECRUIT_FORBIDDEN("RecruitErr: 공고 수정권한 없음", HttpStatus.FORBIDDEN);
 
     private final String message;
     private final HttpStatus status;
