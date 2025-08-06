892a7f0b3b77b502ec9b1aece4b31687f3f3b48a
ysb5397 <ysb5397@gmail.com>
2025-08-06 17:06:54 +0900

    일단 임시로 구독 기능추가

--- src/main/java/com/cloud/cloud_rest/subcorp/SubCorpJpaRepository.java
+++ src/main/java/com/cloud/cloud_rest/subcorp/SubCorpJpaRepository.java
@@ -1,23 +1,15 @@
 package com.cloud.cloud_rest.subcorp;
 
-import com.cloud.cloud_rest.corp.Corp;
-import com.cloud.cloud_rest.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface SubCorpJpaRepository extends JpaRepository <SubCorp ,Long>{
+public interface SubCorpJpaRepository extends JpaRepository <SubCorp, Long>{
 
     @Query("select s from SubCorp s join fetch s.user u where u.id = :userId")
     List<SubCorp> findByUser(@Param("userId") Long userId);
 
-    @Query("SELECT CASE WHEN COUNT(s.user) > 0 THEN true ELSE false END " +
-            "FROM SubCorp s WHERE s.user = :user AND s.corp = :corp")
-    Optional<SubCorp> existsByUserAndCompany(@Param("user") User user, @Param("corp") Corp corp);
-
-    void deleteByUserUserIdAndCorpCorpId(Long userId, Long corpId);
-
+    SubCorp findByUserUserIdAndCorpCorpId(Long userId, Long corpId);
 }
--- src/main/java/com/cloud/cloud_rest/subcorp/SubCorpService.java
+++ src/main/java/com/cloud/cloud_rest/subcorp/SubCorpService.java
@@ -1,6 +1,9 @@
 package com.cloud.cloud_rest.subcorp;
 
+import com.cloud.cloud_rest._global.SessionUser;
 import com.cloud.cloud_rest._global.exception.Exception400;
+import com.cloud.cloud_rest._global.exception.Exception403;
+import com.cloud.cloud_rest._global.exception.Exception404;
 import com.cloud.cloud_rest._global.utils.Define;
 import com.cloud.cloud_rest.corp.Corp;
 import com.cloud.cloud_rest.corp.CorpRepository;
@@ -10,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -23,45 +25,53 @@ public class SubCorpService {
     private final CorpRepository corpRepository;
 
     // 구독 저장 기능
-
     @Transactional
-    public void saveSubCorp(Long userId, Long corpId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> {
+    public SubCorpResponse.SaveDTO saveSubCorp(SubCorpRequest.SaveDTO saveDTO, SessionUser sessionUser) {
+        User user = userRepository.findById(sessionUser.getId()).orElseThrow(() -> {
             throw new Exception400(Define.SaveDTO.ERROR_400);
         });
 
-//
-//         if (subCorpJpaRepository.existsByUserAndCompany(user, orp).isPresent()) {
-//            throw new Exception400(Define.SaveDTO.ERROR_400);
-//        }
+        Corp corp = corpRepository.findById(saveDTO.getCorp().getCorpId())
+                .orElseThrow(() -> new Exception404("존재하지 않는 기업입니다."));
 
+        SubCorp subCorp = subCorpJpaRepository.findByUserUserIdAndCorpCorpId(user.getUserId(), corp.getCorpId());
+
+        if (subCorp == null) {
+            subCorp = saveDTO.toEntity(user, corp);
+            subCorpJpaRepository.save(subCorp);
+            return new SubCorpResponse.SaveDTO(subCorp);
+        } else {
+            throw new Exception400("이미 구독되어 있는 기업입니다.");
+        }
     }
-
-
-
 
     // 구독한 기업 리스트 목록
+    public List<SubCorpResponse.DetailDTO> findAll(Long userId, SessionUser sessionUser) {
+        User user = userRepository.findById(sessionUser.getId())
+                .orElseThrow(() -> new Exception404("유저를 찾을 수 없습니다."));
 
-    public List<SubCorpResponse.SubCorpResponseDTO> getSubscribedCorps(Long userId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> {
-            throw new Exception400(Define.SaveDTO.ERROR_400);
-        });
-
-        List<Corp> corpList = subCorpJpaRepository.findByUser(userId)
-                .stream()
-                .map(SubCorp::getCorp)
-                .toList();
-
-        List<SubCorpResponse.SubCorpResponseDTO> corpInfoList = new ArrayList<>();
-
-        for (Corp corp : corpList) {
-            SubCorpResponse.SubCorpResponseDTO corpLists = new SubCorpResponse.SubCorpResponseDTO(corp);
-            corpInfoList.add(corpLists);
+        if (!userId.equals(user.getUserId())) {
+            throw new Exception403("볼 수 있는 권한이 없습니다.");
         }
 
-        return corpInfoList;
+        List<SubCorp> subCorps = subCorpJpaRepository.findByUser(user.getUserId());
+        return subCorps.stream()
+                .map(SubCorpResponse.DetailDTO::new)
+                .toList();
     }
 
+    // 구독 기업 삭제
+    public void delete(Long id, SessionUser sessionUser) {
+        User user = userRepository.findById(sessionUser.getId())
+                .orElseThrow(() -> new Exception404("유저를 찾을 수 없습니다."));
 
+        SubCorp subCorp = subCorpJpaRepository.findById(id)
+                .orElseThrow(() -> new Exception404("해당 구독 내역을 찾을 수 없습니다"));
 
+        if (!user.getUserId().equals(sessionUser.getId())) {
+            throw new Exception403("삭제할 수 있는 권한이 없습니다.");
+        }
+
+        subCorpJpaRepository.delete(subCorp);
+    }
 }
