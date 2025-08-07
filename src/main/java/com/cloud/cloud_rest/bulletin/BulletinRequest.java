dc4d62ebc8935625109daa50bf178d578f354166
dovahk11m <choongechobiz@gmail.com>
2025-08-07 16:21:26 +0900

    공지사항 전용게시판  Bulletin 패키지 생성/조충희

---
+++ src/main/java/com/cloud/cloud_rest/bulletin/BulletinRequest.java
@@ -0,0 +1,42 @@
+package com.cloud.cloud_rest.bulletin;
+
+import com.cloud.cloud_rest.user.User;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+public class BulletinRequest {
+
+    @Getter
+    @Setter
+    public static class SaveDTO {
+        @NotEmpty
+        private String title;
+
+        @NotEmpty
+        private String content;
+
+        private String base64Image;
+
+        public Bulletin toEntity(User user, String imagePath) {
+            return Bulletin.builder()
+                    .title(this.title)
+                    .content(this.content)
+                    .imagePath(imagePath)
+                    .user(user)
+                    .build();
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class UpdateDTO {
+        @NotEmpty
+        private String title;
+
+        @NotEmpty
+        private String content;
+
+        private String imagePath;
+    }
+}
