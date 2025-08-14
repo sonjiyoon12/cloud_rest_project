23b836588734d5a124efbdec9f8874de89aed9ad
whwwhwjddn123 <whwhwjddn123@gmail.com>
2025-08-14 15:07:52 +0900

    adminResponseDTO 수정

--- src/main/java/com/cloud/cloud_rest/corp_approval/CorpApprovalResponse.java
+++ src/main/java/com/cloud/cloud_rest/corp_approval/CorpApprovalResponse.java
@@ -1,30 +1,33 @@
 package com.cloud.cloud_rest.corp_approval;
 
 import com.cloud.cloud_rest.corp.Corp;
+import com.cloud.cloud_rest.corp.CorpResponse;
 import com.cloud.cloud_rest.corp.CorpStatus;
 import com.cloud.cloud_rest.user.User;
+import com.cloud.cloud_rest.user.UserResponse;
 import lombok.Builder;
 import lombok.Data;
 
 public class CorpApprovalResponse {
 
     @Data
-    public static class ApprovalDTO{
+    public static class ApprovalDTO {
         private Long corpApprovalId;
-        private Corp corp;
-        private User user;
+        private CorpResponse.CorpDTO corp;
+        private UserResponse.UserDTO user;
         private String text;
-        private CorpStatus corpStatus;
+        private String corpStatus;
         private String createdAt;
 
         @Builder
-        public ApprovalDTO (CorpApproval corpApproval){
+        public ApprovalDTO(CorpApproval corpApproval) {
             this.corpApprovalId = corpApproval.getCorpApprovalId();
-            this.corp = corpApproval.getCorp();
-            this.user = corpApproval.getUser();
+            this.corp = new CorpResponse.CorpDTO(corpApproval.getCorp());
+            this.user = new UserResponse.UserDTO(corpApproval.getUser());
             this.text = corpApproval.getText();
-            this.corpStatus = corpApproval.getCorpStatus();
+            this.corpStatus = corpApproval.getCorpStatus().name();
             this.createdAt = corpApproval.getTime();
         }
     }
 }
+
